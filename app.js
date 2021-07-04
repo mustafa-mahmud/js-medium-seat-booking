80bbeba8ccd32d4c9365620a987c628b71d36751
mustafa-mahmud <mithuweb000@gmail.com>
2021-07-04 07:25:34 +0600

    project end without localStorage

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 'use strict';
 
-console.log(123);
+const seats = document.querySelectorAll('.container .row .seat:not(.occupied)');
+const selected = document.getElementById('movie');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+
+const storeIndex = [];
+
+const addClass = (index) => {
+  seats[index].classList.toggle('selected');
+
+  if (storeIndex.indexOf(index) !== -1) {
+    storeIndex.splice(storeIndex.indexOf(index), 1);
+  } else {
+    storeIndex.push(index);
+  }
+
+  selectedValue();
+};
+
+const selectedValue = () => {
+  const value = selected.value;
+
+  count.textContent = storeIndex.length;
+  total.textContent = storeIndex.length * value;
+};
+
+/////////////////////////////////
+////////////////////////////////
+seats.forEach((seat, index) => {
+  seat.addEventListener('click', () => addClass(index));
+});
+
+selected.addEventListener('change', selectedValue);
